3618c1a39796b68a5c93113a967ce549393e3720
ofer221 <ofer2212@gmail.com>
2023-06-30 17:42:24 +0300

    Minor Release 3.3.0

--- client/src/pages/GameWizard/WizardComponents/TargetInput/TargetInput.tsx
+++ client/src/pages/GameWizard/WizardComponents/TargetInput/TargetInput.tsx
@@ -30,8 +30,8 @@ const TargetInput = (props:ITargetInput) => {
             description:target?.description??"",
             hint:{
                 title:target?.name??target?.title??"",
-                text:` לך ל${target?.name??target?.title??""} `,
-                hint:` לך ל${target?.name??target?.title??""} `,
+                text:`${target?.name??target?.title??""} לך ל `,
+                hint:`${target?.name??target?.title??""} לך ל `,
                 image:target?.mapPhotoUrl??""
             },
             id:Math.random().toString(),
--- client/src/pages/GameWizard/WizardComponents/TargetInput/TargetsInputMultiple.tsx
+++ client/src/pages/GameWizard/WizardComponents/TargetInput/TargetsInputMultiple.tsx
@@ -33,8 +33,8 @@ const prevSelectedTargets = useMemo(() => values.map(mission =>mission.target.id
          description:target?.description??"",
          hint:{
              title:target?.name??target?.title??"",
-             text:` לך ל${target?.name??target?.title??""} `,
-             hint:` לך ל${target?.name??target?.title??""} `,
+             text:`${target?.name??target?.title??""} לך ל `,
+             hint:`${target?.name??target?.title??""} לך ל `,
              image:target?.mapPhotoUrl??""
          },
          id:Math.random().toString(),
